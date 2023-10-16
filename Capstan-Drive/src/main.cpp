26af9efd0b38a6cd36bf0b67134f7577903ac269
rohanpatel367 <13253202@student.uts.edu.au>
2023-10-16 18:06:51 +1100

    initial commit - establishing project

---
+++ Capstan-Drive/src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * B-G431B-ESC1 position motion control example with encoder
+ *
+ */
+
+#include <Arduino.h>
+#include <SimpleCAN.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
