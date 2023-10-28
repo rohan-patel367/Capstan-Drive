689e7ba9c9f487cb2f179fd7931b32d8f123d4ac
rohanpatel367 <13253202@student.uts.edu.au>
2023-10-28 11:43:55 +1100

    Basic test with setpoint at starting pos

--- Capstan-Drive/src/main.cpp
+++ Capstan-Drive/src/main.cpp
@@ -1,14 +1,14 @@
-/** 
+/**
  * B-G431B-ESC1 position motion control example with encoder
  *
  */
 #include <SimpleFOC.h>
+#include <QuickPID.h>
 
 // Motor instance
 BLDCMotor motor = BLDCMotor(14);
 BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
-LowsideCurrentSense currentSense = LowsideCurrentSense(0.003f, -64.0f/7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
-
+LowsideCurrentSense currentSense = LowsideCurrentSense(0.003f, -64.0f / 7.0f, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
 
 // encoder instance
 // Encoder encoder = Encoder(A_HALL2, A_HALL3, 2048, A_HALL1);
@@ -17,20 +17,27 @@ MagneticSensorI2C sensor = MagneticSensorI2C(AS5048_I2C);
 // Interrupt routine intialisation
 // channel A and B callbacks
 
+float starting_pos, current_pos, output;
+
+float Kp = 0.25, Ki = 0, Kd = 0;
+float Kp2 = 0.05, Ki2 = 0, Kd2 = 0;
+
+// Specify PID links
+QuickPID myPID(&current_pos, &output, &starting_pos);
 
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.motion(&motor, cmd); }
+void doTarget(char *cmd) { command.motion(&motor, cmd); }
 
-void setup() {
-  
+void setup()
+{
+
   // initialize encoder sensor hardware
   sensor.init();
 
-
   // link the motor to the sensor
   motor.linkSensor(&sensor);
-  
+
   // driver config
   // power supply voltage [V]
   driver.voltage_power_supply = 12;
@@ -52,9 +59,9 @@ void setup() {
   motor.velocity_index_search = 3;
 
   // set motion control loop to be used
-  motor.controller = MotionControlType::angle;
+  motor.controller = MotionControlType::torque;
 
-  // contoller configuration 
+  // contoller configuration
   // default parameters in defaults.h
 
   // velocity PI controller parameters
@@ -65,7 +72,7 @@ void setup() {
   // jerk control using voltage voltage ramp
   // default value is 300 volts per sec  ~ 0.3V per millisecond
   motor.PID_velocity.output_ramp = 1000;
- 
+
   // velocity low pass filtering time constant
   motor.LPF_velocity.Tf = 0.01;
 
@@ -73,15 +80,14 @@ void setup() {
   motor.P_angle.P = 20;
   //  maximal velocity of the position control
   motor.velocity_limit = 80;
-;
+  ;
   motor.current_limit = 2;
 
-
-  // use monitoring with serial 
+  // use monitoring with serial
   Serial.begin(115200);
   // comment out if not needed
   motor.useMonitoring(Serial);
-  
+
   // initialize motor
   motor.init();
   // align encoder and start FOC
@@ -93,18 +99,53 @@ void setup() {
   Serial.println(F("Motor ready."));
   Serial.println(F("Set the target angle using serial terminal:"));
   _delay(1000);
+  sensor.update();
+  starting_pos = 0;
+  myPID.SetTunings(Kp, Ki, Kd);
+  myPID.SetMode(myPID.Control::automatic);
+  myPID.SetOutputLimits(0.3, 10);
 }
 
-void loop() {
+void loop()
+{
   // main FOC algorithm function
 
   // Motion control function
   motor.move();
-  //sensor.update();
-  //Serial.println(sensor.getAngle());
-  // function intended to be used with serial plotter to monitor motor variables
-  // significantly slowing the execution down!!!!
-  // motor.monitor();
+  // sensor.update();
+  current_pos = -abs(sensor.getAngle());
+  
+  // Serial.println(starting_pos);
+  // Serial.println(current_pos);
+  // Serial.println(output);
+  // Serial.println(sensor.getAngle() - starting_pos);
+  // Serial.println();
+  // if (abs(sensor.getAngle() - starting_pos) < 0.2)
+  // {
+  //     myPID.SetTunings(Kp2, Ki2, Kd2);
+  // }
+  // else {
+  //     myPID.SetTunings(Kp, Ki, Kd);
+  // }
+   myPID.Compute();
+
+  if(abs(sensor.getAngle() - starting_pos) < 0.3){
+    output = 0;
+  }
+  if (sensor.getAngle() - starting_pos > 0)
+  {
+    Serial.println(-output);
+    motor.target = -output;
+  }
+  else
+  {
+    Serial.println(output);
+    motor.target = output;
+  }
+  // motor.target = -(sensor.getAngle() - starting_pos) * 0.3;
+  //  function intended to be used with serial plotter to monitor motor variables
+  //  significantly slowing the execution down!!!!
+  //  motor.monitor();
   motor.loopFOC();
   // user communication
   command.run();
